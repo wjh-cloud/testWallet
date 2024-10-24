5083e4a65b41f2b9e0939f26b8839cf76d1f642e
ggg <wjhjavahaha@163.com>
2024-10-24 11:49:41 +0800

    chanag    sdasdsa

---
+++ tma-usdt-payments-demo/src/constants/common-constants.ts
@@ -0,0 +1,4 @@
+import { Address } from '@ton/core';
+
+export const USDT_MASTER_ADDRESS = Address.parse('kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy');
+export const INVOICE_WALLET_ADDRESS = Address.parse('0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-');
---
+++ tma-usdt-payments-demo/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tsconfigPaths()],
+  publicDir: './public',
+  build: {
+    outDir: 'docs'
+  },
+});
+
